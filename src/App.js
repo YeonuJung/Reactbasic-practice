4dbeb2c6bab979a438015c43e7cc177afaf93ef9
YeonuJung <gsh95214@naver.com>
2024-03-15 15:36:19 +0900

    리액트 연습

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 /* import Hello from './components/Hello';*/
-import TodaysPlan from './components/TodaysPlan';
+// import TodaysPlan from './components/TodaysPlan';
+import HeaderComponent from './components/HeaderComponent';
+import BodyComponent from './components/BodyComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
     <>
     {/* <Hello/> */}
-    <TodaysPlan/>
+    {/* <TodaysPlan/> */}
+    <HeaderComponent/>
+    <BodyComponent/>
+    <FooterComponent/>
     </>
   );
 }
